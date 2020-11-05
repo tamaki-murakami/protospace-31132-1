e4fa360f998fe9bf59445809fea883f340d3004e
tamaki-murakami <72539750+tamaki-murakami@users.noreply.github.com>
2020-11-05 15:14:28 +0900

    コントローラーとアクション、ルーティングを設定しましょう

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes
+  resources :prototypes do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
   root to: "prototypes#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
