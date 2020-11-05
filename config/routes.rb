3b2f5d771e5aefd16d8c429636f4036e4d94d773
tamaki-murakami <72539750+tamaki-murakami@users.noreply.github.com>
2020-11-05 14:13:59 +0900

    詳細ページで投稿の情報が表示されるようにしましょう

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :prototypes, only: [:index, :new, :create]
+  resources :prototypes, only: [:index, :new, :create, :show]
   root to: "prototypes#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
